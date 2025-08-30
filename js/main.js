4ac51e7d14299859215af0201d8a840bd42a71df
menhunging <menhunging@gmail.com>
2025-08-30 12:04:30 +0300

    add twentytwenty

--- js/main.js
+++ js/main.js
@@ -8,6 +8,21 @@ addEventListener("scroll", (event) => {
   }
 });
 
+addEventListener("load", (event) => {
+  if ($(".twentytwenty-block").length > 0) {
+    $(".twentytwenty-block").twentytwenty({
+      default_offset_pct: 0.5, // How much of the before image is visible when the page loads
+      orientation: "horizontal", // Orientation of the before and after images ('horizontal' or 'vertical')
+      no_overlay: true, //Do not show the overlay with before and after
+      move_slider_on_hover: false, // Move slider on mouse hover?
+      move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement.
+      click_to_move: false, // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+    });
+
+    $(".twentytwenty-block").addClass("load");
+  }
+});
+
 $(document).ready(function () {
   if ($(".burger").length > 0) {
     let burger = $(".burger");
